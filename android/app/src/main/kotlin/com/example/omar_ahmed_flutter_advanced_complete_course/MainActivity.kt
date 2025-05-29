fad9898b03ac7785326873d23f3f6b470c016946
eyadabbod55 <eyadabbod55@gmail.com>
2025-05-29 21:46:21 +0300

    Lanch app

---
+++ android/app/src/main/kotlin/com/example/omar_ahmed_flutter_advanced_complete_course/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.omar_ahmed_flutter_advanced_complete_course
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:omar_ahmed_flutter_advanced_complete_course/core/routing/app_router.dart';
+import 'package:omar_ahmed_flutter_advanced_complete_course/doc_app.dart';
+
+void main() {
+  runApp(DocApp(appRouter: AppRouter()));
+}
